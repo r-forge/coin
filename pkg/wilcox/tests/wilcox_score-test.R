b678506ec5389cd47640dc6cd592b97e1be4137a
thothorn (Torsten Hothorn) <Torsten.Hothorn@R-project.org>
2022-04-27 09:47:09 +0000

    examples
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@2068 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/wilcox/tests/wilcox_score-test.R
+++ pkg/wilcox/tests/wilcox_score-test.R
@@ -6,3 +6,13 @@ y <- rnorm(50)
 
 .wilcox_score(x, y)
 
+x <- rnorm(500)
+y <- rnorm(500)
+
+.wilcox_score(x, y, exact = FALSE)
+
+x <- rnorm(5000)
+y <- rnorm(5000)
+
+.wilcox_score(x, y, exact = FALSE)
+
