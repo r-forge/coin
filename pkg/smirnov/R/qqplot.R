abfee60c3ee4566ccf65b6d585c4a0b8af6f2976
thothorn (Torsten Hothorn) <Torsten.Hothorn@R-project.org>
2022-05-24 13:58:57 +0000

    conf.args
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@2083 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/smirnov/R/qqplot.R
+++ pkg/smirnov/R/qqplot.R
@@ -2,8 +2,9 @@
 qqplot <- function(x, y, plot.it = TRUE,
                    xlab = deparse1(substitute(x)),
                    ylab = deparse1(substitute(y)), ..., 
-                   conf.level = NULL, exact = NULL, 
-                   simulate = FALSE, B = 2000, col = NA)
+                   conf.level = NULL, 
+                   conf.args = list(exact = NULL, simulate.p.value = FALSE,
+                                    B = 2000, col = NA, border = NULL))
 {
     sx <- sort(x)
     sy <- sort(y)
@@ -24,13 +25,21 @@ qqplot <- function(x, y, plot.it = TRUE,
             stop("'conf.level' is not a probability")
 
     N <- lenx + leny
+    if (is.null(conf.args$exact)) conf.args$exact <- NULL
+    exact <- conf.args$exact
+    if (is.null(conf.args$simulate.p.value)) conf.args$simulate.p.value <- FALSE
+    simulate <- conf.args$simulate.p.value
+    if (is.null(conf.args$B)) conf.args$B <- 2000
+    if (is.null(conf.args$col)) conf.args$col <- NA
+    if (is.null(conf.args$border)) conf.args$border <- NULL
+
     if (is.null(exact)) exact <- lenx * leny < 10000
     z <- c(x, y)
     TIES <- any(duplicated(z))
     if (!TIES) z <- NULL
     ca <- qsmirnov(conf.level, sizes = c(lenx, leny), z = z,
                    exact = exact && !simulate, 
-                   simulate = simulate, B = B)
+                   simulate = simulate, B = conf.args$B)
 
     ### Switzer (1976, Biometrika) 10.1093/biomet/63.1.13
     i <- as.double(seq_along(x))
@@ -59,7 +68,8 @@ qqplot <- function(x, y, plot.it = TRUE,
         y <- y[!is.na(y)]
         xn <- c(x[1L], rep(x[-1L], each = 2))
         yn <- c(rep(y[-length(y)], each = 2), y[length(y)])
-        polygon(x = xn, y = yn, col = col, ...)
+        polygon(x = xn, y = yn, col = conf.args$col,
+                border = conf.args$border)
         points(sx, sy, ...)
     }	
 
