2f91a377078c39f5190b65159fe44159854ccb27
thothorn (Torsten Hothorn) <Torsten.Hothorn@R-project.org>
2024-04-29 16:25:41 +0000

    update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@2213 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www_src/config.R
+++ www_src/config.R
@@ -3,6 +3,6 @@ pkg <- "coin"
 dest <- "html"
 publish <- "../www"
 
-download.file("http://user.math.uzh.ch/hothorn/TH.bib", dest = "TH.bib")
+download.file("http://user.math.uzh.ch/hothorn/bib/TH.bib", dest = "TH.bib")
 system("cat coin.bib >> TH.bib")
 bib <- "TH.bib"
