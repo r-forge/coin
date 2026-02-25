bce7f0d718cabaa6c6aec1892f25cb6d12c784fa
thothorn (Torsten Hothorn) <Torsten.Hothorn@R-project.org>
2026-02-25 16:17:02 +0000

    prepare for CRAN
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/coin@2506 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/free1way/tests/free1way-Ex.R
@@ -0,0 +1,208 @@
+pkgname <- "free1way"
+source(file.path(R.home("share"), "R", "examples-header.R"))
+options(warn = 1)
+library('free1way')
+
+base::assign(".oldSearch", base::search(), pos = 'CheckExEnv')
+base::assign(".old_wd", base::getwd(), pos = 'CheckExEnv')
+cleanEx()
+nameEx("free1way")
+### * free1way
+
+flush(stderr()); flush(stdout())
+
+### Name: free1way
+### Title: Distribution-free Inference in a Stratified One-Way Layout
+### Aliases: free1way free1way.formula free1way.table free1way.numeric
+###   free1way.factor print.free1way coef.free1way vcov.free1way
+###   logLik.free1way summary.free1way confint.free1way
+### Keywords: htest
+
+### ** Examples
+
+
+## Kruskal-Wallis test
+kruskal.test(Ozone ~ Month, data = airquality)
+kt <- free1way(Ozone ~ Month, data = airquality)
+print(kt)
+# log-odds ratios for comparison with control
+coef(kt)
+# Wald inference
+summary(kt)
+confint(kt, test = "Wald")
+
+## Friedman test
+example(friedman.test, echo = FALSE)
+me <- colnames(RoundingTimes)
+d <- expand.grid(me = factor(me, labels = me, levels = me),
+                 id = factor(seq_len(nrow(RoundingTimes))))
+d$time <- c(t(RoundingTimes))
+# global p-value identical
+friedman.test(RoundingTimes)
+ft <- free1way(time ~ me | id, data = d)
+print(ft)
+coef(ft)
+# Wald inference
+summary(ft)
+confint(ft, test = "Wald")
+
+## McNemar test
+## paired binary observations
+example(mcnemar.test, echo = FALSE)
+# set-up data frame with survey outcomes for voters
+s <- gl(2, 1, labels = dimnames(Performance)[[1L]])
+survey <- gl(2, 1, labels = c("1st", "2nd"))
+nvoters <- c(Performance)
+x <- expand.grid(survey = survey, voter = factor(seq_len(sum(nvoters))))
+x$performance <- c(rep(s[c(1, 1)], nvoters[1]), rep(s[c(2, 1)], nvoters[2]),
+                   rep(s[c(1, 2)], nvoters[3]), rep(s[c(2, 2)], nvoters[4]))
+# note that only those voters changing their minds are relevant
+mcn <- free1way(xtabs(~ performance + survey + voter, data = x))
+# same result as mcnemar.test w/o continuity correction
+print(mcn)
+# X^2 statistic
+summary(mcn, test = "Permutation")$statistic^2
+mcnemar.test(Performance, correct = FALSE)
+# Wald inference
+summary(mcn)
+confint(mcn, test = "Wald")
+
+## Mantel-Haenszel test w/o continuity correction, 
+## Departments are blocks
+mantelhaen.test(UCBAdmissions, correct = FALSE)
+mh <- free1way(UCBAdmissions)
+print(mh)
+# common odds-ratio, with score interval
+exp(coef(mh))
+exp(confint(mh, test = "Rao"))
+# looking at department-specific 
+# confidence intervals for log-odds ratios 
+# it seems Dept A is out of line
+apply(UCBAdmissions, MARGIN = 3,  
+      FUN = function(x) confint(free1way(as.table(x))))
+
+## Mantel-Haenszel test treats variables as
+## unordered, free1way allows ordered responses
+example(mantelhaen.test, echo = FALSE)
+# Does distribution of job satisfaction (ordered) depend on income
+# in a stratified proportional odds model?
+# Job Satisfaction is second in array but needs to be first
+# for free1way to treat it as ordered response
+ft <- free1way(aperm(Satisfaction, perm = c(2, 1, 3)))
+summary(ft)
+
+
+
+
+cleanEx()
+nameEx("power.free1way.test")
+### * power.free1way.test
+
+flush(stderr()); flush(stdout())
+
+### Name: power.free1way.test
+### Title: Power Calculations for Distribution-free Wald Tests in
+###   Stratified One-Way Layouts
+### Aliases: rfree1way power.free1way.test
+### Keywords: htest
+
+### ** Examples
+
+
+## make example reproducible
+if (!exists(".Random.seed", envir = .GlobalEnv, inherits = FALSE)) 
+    runif(1)
+R.seed <- get(".Random.seed", envir = .GlobalEnv)
+set.seed(29)
+
+## sample from proportional odds model with 1:2 allocation
+## based on odds ratio of 3, with sample sizes (15, 30)
+x <- rfree1way(n = 15, delta = log(3), alloc_ratio = 2)
+
+# Wilcoxon-Mann-Whitney rank sum test via classical stats interface
+wilcox.test(y ~ groups, data = x, exact = FALSE, correct = FALSE)$p.value
+
+# Identical p-value obtained from a proportional-odds model 
+summary(free1way(y ~ groups, data = x), test = "Permutation")$p.value
+
+# approximate power for this test
+power.free1way.test(n = 15, delta = log(3), alloc_ratio = 2)
+
+assign(".Random.seed", R.seed, envir = .GlobalEnv)
+
+
+
+
+cleanEx()
+nameEx("ppplot")
+### * ppplot
+
+flush(stderr()); flush(stdout())
+
+### Name: ppplot
+### Title: Probability-probability Plots
+### Aliases: ppplot
+### Keywords: hplot distribution
+
+### ** Examples
+
+
+## make example reproducible
+if (!exists(".Random.seed", envir = .GlobalEnv, inherits = FALSE)) 
+    runif(1)
+R.seed <- get(".Random.seed", envir = .GlobalEnv)
+set.seed(29)
+
+## well-fitting logistic model
+nd <- data.frame(groups = gl(2, 50, labels = paste0("G", 1:2)))
+nd$y <- rlogis(nrow(nd), location = c(0, 2)[nd$groups])
+with(with(nd, split(y, groups)),
+     ppplot(G1, G2, conf.level = .95,
+            conf.args = list(link = "logit", type = "Wald", col = 2)))
+# with appropriate Wilcoxon test and log-odds ratio
+coef(ft <- free1way(y ~ groups, data = nd))
+# the model-based probability-probability curve
+prb <- 1:99 / 100
+points(prb,  plogis(qlogis(prb) - coef(ft)), pch = 3)
+
+## the corresponding model-based receiver operating characteristic (ROC)
+## curve, see Sewak and Hothorn (2023)
+plot(prb,  plogis(qlogis(1 - prb) - coef(ft), lower.tail = FALSE),
+     xlab = "1 - Specificity", ylab = "Sensitivity", type = "l", 
+     main = "ROC Curve")
+abline(a = 0, b = 1, col = "lightgrey")
+# with confidence band
+lines(prb, plogis(qlogis(1 - prb) - confint(ft, test = "Rao")[1], 
+      lower.tail = FALSE), lty = 3)
+lines(prb, plogis(qlogis(1 - prb) - confint(ft, test = "Rao")[2], 
+      lower.tail = FALSE), lty = 3)
+# and corresponding area under the ROC curve (AUC)
+# with score confidence interval
+coef(ft, what = "AUC")
+confint(ft, test = "Rao", what = "AUC")
+
+## ill-fitting normal model
+nd$y <- rnorm(nrow(nd), mean = c(0, .5)[nd$groups], sd = c(1, 1.5)[nd$groups])
+with(with(nd, split(y, groups)),
+     ppplot(G1, G2, conf.level = .95,
+            conf.args = list(link = "probit", type = "Wald", col = 2)))
+# inappropriate probit model
+coef(free1way(y ~ groups, data = nd, link = "probit"))
+
+assign(".Random.seed", R.seed, envir = .GlobalEnv)
+
+
+
+
+### * <FOOTER>
+###
+cleanEx()
+options(digits = 7L)
+base::cat("Time elapsed: ", proc.time() - base::get("ptime", pos = 'CheckExEnv'),"\n")
+grDevices::dev.off()
+###
+### Local variables: ***
+### mode: outline-minor ***
+### outline-regexp: "\\(> \\)?### [*]+" ***
+### End: ***
+quit('no')
